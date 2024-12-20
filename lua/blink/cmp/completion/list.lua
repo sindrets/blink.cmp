35aeb87e9ca0a03b738822f21c5557baf59c33a4
Sindre T. Strøm <sindrets@gmail.com>
2024-12-20 12:44:46 +0100

    fix: use same logic for unselecting when moving out of list bounds
    `select_{next|prev}` behaved differently here. Might have been
    intentional, but I didn't like it.

--- lua/blink/cmp/completion/list.lua
+++ lua/blink/cmp/completion/list.lua
@@ -174,8 +174,8 @@ function list.select_prev(insert)
     -- cycling around has been disabled, ignore
     if not list.config.cycle.from_top then return end
 
-    -- auto_insert is enabled, we go back to no selection
-    if list.config.selection == 'auto_insert' then return list.select(nil, select_opts) end
+    -- preselect is not enabled, we go back to no selection
+    if list.config.selection ~= 'preselect' then return list.select(nil, select_opts) end
 
     -- otherwise, we cycle around
     return list.select(#list.items, select_opts)
